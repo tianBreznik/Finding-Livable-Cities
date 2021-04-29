3f4f3b214a95a0f963c899e1eee7ee4e2a981f7e
tianBreznik <37045546+tianBreznik@users.noreply.github.com>
2021-04-29 22:31:14 +0200

    Updates

---
+++ precache-manifest.4828829d25ae728037df1727d2808557.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b04a8f9f4cd4e1984748",
+    "url": "/finding-livable-cities/static/css/main.f67877ca.chunk.css"
+  },
+  {
+    "revision": "b04a8f9f4cd4e1984748",
+    "url": "/finding-livable-cities/static/js/main.b04a8f9f.chunk.js"
+  },
+  {
+    "revision": "d26818c64503da8e57cc",
+    "url": "/finding-livable-cities/static/js/1.d26818c6.chunk.js"
+  },
+  {
+    "revision": "cff7c018d705b024b203",
+    "url": "/finding-livable-cities/static/js/runtime~main.cff7c018.js"
+  },
+  {
+    "revision": "4ee61d6819baaaccd8999a5f16b0d1fd",
+    "url": "/finding-livable-cities/index.html"
+  }
+];
